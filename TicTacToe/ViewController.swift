e35d2334b35b9fd6dd84db9647a344366eb80b04
Sagitova Gulnaz <s58888@beuth-hochschule.de>
2016-11-16 00:09:29 +0100

    added tag to Images, reworked GestureRecognizer assigment

--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -23,28 +23,17 @@ class ViewController: UIViewController {
     
     @IBOutlet var msg: UILabel!
     
-    let tapGestureRecognizer11 = UITapGestureRecognizer()
-    let tapGestureRecognizer12 = UITapGestureRecognizer()
-    let tapGestureRecognizer13 = UITapGestureRecognizer()
-    let tapGestureRecognizer21 = UITapGestureRecognizer()
-    let tapGestureRecognizer22 = UITapGestureRecognizer()
-    let tapGestureRecognizer23 = UITapGestureRecognizer()
-    let tapGestureRecognizer31 = UITapGestureRecognizer()
-    let tapGestureRecognizer32 = UITapGestureRecognizer()
-    let tapGestureRecognizer33 = UITapGestureRecognizer()
+    var images = [UIImageView]()
     
-    var player:Dictionary = ["first":1,"second":2]
-    
-    
-    
-    func tappedView(){
-        print("image tapped")
+    func tappedView(sender: UIGestureRecognizer){
+        let image = sender.view as? UIImageView
         msg.text = "You tapped the box!"
+        let n = (image?.tag)! as Int
+        msg.text = "tag \(n)"
     }
     
-    @IBAction func newGame(_ sender: AnyObject) {
+    @IBAction func newGame(_ sender: UIButton) {
         msg.text = "New Game!"
-        
     }
     @IBAction func resetGame(_ sender: AnyObject) {
         msg.text = "reset"
@@ -52,38 +41,20 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        a11.isUserInteractionEnabled = true;
-        a12.isUserInteractionEnabled = true;
-        a13.isUserInteractionEnabled = true;
-        a21.isUserInteractionEnabled = true;
-        a22.isUserInteractionEnabled = true;
-        a23.isUserInteractionEnabled = true;
-        a31.isUserInteractionEnabled = true;
-        a32.isUserInteractionEnabled = true;
-        a33.isUserInteractionEnabled = true;
+        
+        images = [a11, a12, a13, a21, a22, a23, a31, a32, a33]
+//        var tag = 0
+        for image in images {
+            let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.tappedView))
+            image.isUserInteractionEnabled = true
+            image.addGestureRecognizer(tapGestureRecognizer)
+//            image.tag = tag
+//            tag += 1
+        }
+        
+        
 
         
-        tapGestureRecognizer11.addTarget(self, action: #selector(ViewController.tappedView))
-        tapGestureRecognizer12.addTarget(self, action: #selector(ViewController.tappedView))
-        tapGestureRecognizer13.addTarget(self, action: #selector(ViewController.tappedView))
-        tapGestureRecognizer21.addTarget(self, action: #selector(ViewController.tappedView))
-        tapGestureRecognizer22.addTarget(self, action: #selector(ViewController.tappedView))
-        tapGestureRecognizer23.addTarget(self, action: #selector(ViewController.tappedView))
-        tapGestureRecognizer31.addTarget(self, action: #selector(ViewController.tappedView))
-        tapGestureRecognizer32.addTarget(self, action: #selector(ViewController.tappedView))
-        tapGestureRecognizer33.addTarget(self, action: #selector(ViewController.tappedView))
-
-        a11.addGestureRecognizer(tapGestureRecognizer11)
-        a12.addGestureRecognizer(tapGestureRecognizer12)
-        a13.addGestureRecognizer(tapGestureRecognizer13)
-        a21.addGestureRecognizer(tapGestureRecognizer21)
-        a22.addGestureRecognizer(tapGestureRecognizer22)
-        a23.addGestureRecognizer(tapGestureRecognizer23)
-        a31.addGestureRecognizer(tapGestureRecognizer31)
-        a32.addGestureRecognizer(tapGestureRecognizer32)
-        a33.addGestureRecognizer(tapGestureRecognizer33)
-
     }
 
     override func didReceiveMemoryWarning() {
@@ -91,6 +62,6 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    
 }
 
