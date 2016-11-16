1581c4a8ad5c35ceb86ffd7f80c13866841cd332
Sagitova Gulnaz <s58888@beuth-hochschule.de>
2016-11-17 00:20:28 +0100

    added class Player,new function -  split tag for images

--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -20,40 +20,74 @@ class ViewController: UIViewController {
     @IBOutlet var a31: UIImageView!
     @IBOutlet var a32: UIImageView!
     @IBOutlet var a33: UIImageView!
-    
     @IBOutlet var msg: UILabel!
-    
     var images = [UIImageView]()
+    enum move:String {
+        case x = "x"
+        case o = "o"
+    }
+    let player1 = Player(name: "First", way: move.x.rawValue)
+    let player2 = Player(name: "Second", way: move.o.rawValue)
+    var currentPlayer = Player(name: "dummyPlayer", way: move.x.rawValue)
+
     
     func tappedView(sender: UIGestureRecognizer){
-        let image = sender.view as? UIImageView
-        msg.text = "You tapped the box!"
-        let n = (image?.tag)! as Int
-        msg.text = "tag \(n)"
+        
+        if currentPlayer  === player1 {
+            msg.text = "The first Player turn!"
+            let image = sender.view as? UIImageView
+            image?.image = UIImage(named: "x")
+            let n = (image?.tag)! as Int
+            print("tag \(n)")
+            splitTag(value: n)
+//            checkForWin(value: n)
+            currentPlayer = player2
+        } else if currentPlayer === player2{
+            msg.text = "The second player turn!"
+            let image = sender.view as? UIImageView
+            image?.image = UIImage(named: "o")
+            let n = (image?.tag)! as Int
+            print("tag \(n)")
+            splitTag(value: n)
+//            checkForWin(value: n)
+            currentPlayer = player1
+        }
     }
     
+    func splitTag(value:Int) -> (x:Int, y:Int){
+        let x:Int = value % 10
+        print(x)
+        let y:Int = (value - x) / 10
+        print(y)
+        return (x, y)
+    }
+    
+//    func checkForWin(value:Int) -> String{
+//        print(n)
+//        if drawX {
+//            
+//            msg.text = "the first player won!!!"
+//        } else {
+//            msg.text = "the second player won!!!"
+//        }
+//    }
     @IBAction func newGame(_ sender: UIButton) {
         msg.text = "New Game!"
+        for image in images {
+            image.image = nil
+        }
     }
-    @IBAction func resetGame(_ sender: AnyObject) {
-        msg.text = "reset"
-    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        currentPlayer = player1
         images = [a11, a12, a13, a21, a22, a23, a31, a32, a33]
-//        var tag = 0
         for image in images {
             let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.tappedView))
             image.isUserInteractionEnabled = true
             image.addGestureRecognizer(tapGestureRecognizer)
-//            image.tag = tag
-//            tag += 1
         }
-        
-        
-
         
     }
 
@@ -61,7 +95,15 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    class Player{
+        var name:String = ""
+        var way = move.x
+        
+        init(name:String, way:String){
+            self.name = name
+            self.way = move.x
+        }
+    }
     
 }
 
